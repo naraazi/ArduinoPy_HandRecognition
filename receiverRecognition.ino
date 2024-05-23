48dee3eaad78e65a8a70be63f624403b42930c9a
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2024-05-23 13:18:26 -0300

    Arduino code now compare strings, not bytes

--- receiverRecognition.ino
+++ receiverRecognition.ino
@@ -1,5 +1,7 @@
 void setup() {
     Serial.begin(9600);
+    Serial.setTimeout(100);
+
     pinMode(8, OUTPUT);
     pinMode(9, OUTPUT);
     pinMode(10, OUTPUT);
@@ -10,35 +12,34 @@ void setup() {
     digitalWrite(10, LOW);
     digitalWrite(11, LOW);
     digitalWrite(12, LOW);
-
 }
 
 void loop() {
     if (Serial.available() > 0) {
-        int quant = Serial.available();
-        String teststr = Serial.readStringUntil("\r");  // -- read until timeout - remove any \r \n whitespace at the end of the String
+        String teststr = Serial.readString();  // -- read until timeout
+        teststr.trim();  // -- remove any \r \n whitespace at the end of the String
 
-        if (quant == 1) {
+        if (teststr.equals("a")) {
             digitalWrite(8, HIGH);
             delay(1000);
             digitalWrite(8, LOW);
-            }
-        if (quant == 2) {
+        }
+        if (teststr.equals("b")) {
             digitalWrite(9, HIGH);
             delay(1000);
             digitalWrite(9, LOW);
         }
-        if (quant == 3) {
+        if (teststr.equals("c")) {
             digitalWrite(10, HIGH);
             delay(1000);
             digitalWrite(10, LOW);
         }
-        if (quant == 4) {
+        if (teststr.equals("d")) {
             digitalWrite(11, HIGH);
             delay(1000);
             digitalWrite(11, LOW);
         }
-        if (quant == 5) {
+        if (teststr.equals("e")) {
             digitalWrite(12, HIGH);
             delay(1000);
             digitalWrite(12, LOW);
