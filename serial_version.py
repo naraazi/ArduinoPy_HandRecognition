d57dce42c16026e9d0519ca84292e50c3862199e
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2024-05-23 13:16:57 -0300

    Serial code now exchange strings, not bytes

--- serial_version.py
+++ serial_version.py
@@ -9,74 +9,81 @@ sleep(2)
 detector = HandDetector(detectionCon=0.8, maxHands=1)
 video = cv2.VideoCapture(0)  # -- modify the camera entry point here
 
+previous_fingerUp = None  # -- variable to store the previous gesture
+
 while True:
     ret, frame = video.read()
     frame = cv2.flip(frame, 1)
     hands, img = detector.findHands(frame)
+
     if hands:
         lmList = hands[0]
         fingerUp = detector.fingersUp(lmList)
 
-        print(fingerUp)
-        if fingerUp == [0, 0, 0, 0, 0]:  # -- no finger up
-            cv2.putText(frame,
-                        'Nenhum dedo!',
-                        (20, 460),
-                        cv2.FONT_HERSHEY_COMPLEX,
-                        1,
-                        (255, 255, 255),
-                        1,
-                        cv2.LINE_AA)
-        elif fingerUp == [0, 1, 0, 0, 0]:  # -- index finger up
-            cv2.putText(frame,
-                        'Um dedo!',
-                        (20, 460),
-                        cv2.FONT_HERSHEY_COMPLEX,
-                        1,
-                        (255, 255, 255),
-                        1,
-                        cv2.LINE_AA)
-            ser.write(b'a')
-        elif fingerUp == [0, 1, 1, 0, 0]:  # -- middle finger up
-            cv2.putText(frame,
-                        'Dois dedos!',
-                        (20, 460),
-                        cv2.FONT_HERSHEY_COMPLEX,
-                        1,
-                        (255, 255, 255),
-                        1,
-                        cv2.LINE_AA)
-            ser.write(b'ab')
-        elif fingerUp == [0, 1, 1, 1, 0]:  # -- ring finger up
-            cv2.putText(frame,
-                        'Três dedos!',
-                        (20, 460),
-                        cv2.FONT_HERSHEY_COMPLEX,
-                        1,
-                        (255, 255, 255),
-                        1,
-                        cv2.LINE_AA)
-            ser.write(b'abc')
-        elif fingerUp == [0, 1, 1, 1, 1]:  # -- little finger up
-            cv2.putText(frame,
-                        'Quatro dedos!',
-                        (20, 460),
-                        cv2.FONT_HERSHEY_COMPLEX,
-                        1,
-                        (255, 255, 255),
-                        1,
-                        cv2.LINE_AA)
-            ser.write(b'abcd')
-        elif fingerUp == [1, 1, 1, 1, 1]:  # -- thumb up
-            cv2.putText(frame,
-                        'Cinco dedos!',
-                        (20, 460),
-                        cv2.FONT_HERSHEY_COMPLEX,
-                        1,
-                        (255, 255, 255),
-                        1,
-                        cv2.LINE_AA)
-            ser.write(b'abcde')
+        if fingerUp != previous_fingerUp:  # -- check if the gesture has changed
+            previous_fingerUp = fingerUp  # -- update the previous gesture
+
+            if fingerUp == [0, 0, 0, 0, 0]:  # -- no finger up
+                cv2.putText(frame,
+                            'Nenhum dedo!',
+                            (20, 460),
+                            cv2.FONT_HERSHEY_COMPLEX,
+                            1,
+                            (255, 255, 255),
+                            1,
+                            cv2.LINE_AA)
+            elif fingerUp == [0, 1, 0, 0, 0]:  # -- index finger up
+                cv2.putText(frame,
+                            'Um dedo!',
+                            (20, 460),
+                            cv2.FONT_HERSHEY_COMPLEX,
+                            1,
+                            (255, 255, 255),
+                            1,
+                            cv2.LINE_AA)
+                ser.write('a'.encode('utf-8'))
+            elif fingerUp == [0, 1, 1, 0, 0]:  # -- middle finger up
+                cv2.putText(frame,
+                            'Dois dedos!',
+                            (20, 460),
+                            cv2.FONT_HERSHEY_COMPLEX,
+                            1,
+                            (255, 255, 255),
+                            1,
+                            cv2.LINE_AA)
+                ser.write('b'.encode('utf-8'))
+            elif fingerUp == [0, 1, 1, 1, 0]:  # -- ring finger up
+                cv2.putText(frame,
+                            'Três dedos!',
+                            (20, 460),
+                            cv2.FONT_HERSHEY_COMPLEX,
+                            1,
+                            (255, 255, 255),
+                            1,
+                            cv2.LINE_AA)
+                ser.write('c'.encode('utf-8'))
+            elif fingerUp == [0, 1, 1, 1, 1]:  # -- little finger up
+                cv2.putText(frame,
+                            'Quatro dedos!',
+                            (20, 460),
+                            cv2.FONT_HERSHEY_COMPLEX,
+                            1,
+                            (255, 255, 255),
+                            1,
+                            cv2.LINE_AA)
+                ser.write('d'.encode('utf-8'))
+            elif fingerUp == [1, 1, 1, 1, 1]:  # -- thumb up
+                cv2.putText(frame,
+                            'Cinco dedos!',
+                            (20, 460),
+                            cv2.FONT_HERSHEY_COMPLEX,
+                            1,
+                            (255, 255, 255),
+                            1,
+                            cv2.LINE_AA)
+                ser.write('e'.encode('utf-8'))
+    else:
+        previous_fingerUp = None  # -- reset previous gesture when no hands are detected
 
     cv2.imshow("Retorno", frame)
     k = cv2.waitKey(1)
